68f8c1ce17d30b28f34ff16a6f0e9553cf349b3b
George Dimitrov <george.dimitrov@ludotech.co>
2023-01-06 14:21:34 +0100

    console working hangman

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,84 @@
-mod addition;
 use rand::Rng;
-use std::io;
+use std::io::{self, Write};
 
+mod tests;
 fn main() {
-    let mut maximum_player_lives: i8 = 6;
-
-    const POSSIBLE_WORDS_TO_GUESS: &[&str] = &["pineapple", "water", "milk"];
+    const POSSIBLE_WORDS_TO_GUESS: &[&str] = &[
+        "pineapple",
+        "water",
+        "milk",
+        "pencil",
+        "pen",
+        "dog",
+        "cat",
+        "sofa",
+        "javascript",
+    ];
 
     const WELCOME_MESSAGE: &'static str =
         "Welcome to the hangman guessing game mate, good luck or get hanged!";
 
     println!("{WELCOME_MESSAGE}");
 
-    let mut user_input = String::new();
-
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line");
-
-    println!("User input is: {user_input }");
-
     start_game(POSSIBLE_WORDS_TO_GUESS)
 }
 
 fn start_game(words: &[&str]) {
-    let random_generated_number = rand::thread_rng().gen_range(0..=words.len() - 1);
+    let mut maximum_player_lives: i8 = 6;
+    println!("Remaining lives: {maximum_player_lives}");
 
-    println!("Provided array {:?}:", words);
-    println!("Random number is: {random_generated_number}");
+    let random_generated_number = rand::thread_rng().gen_range(0..=words.len() - 1);
+    let word_to_be_guessed = words[random_generated_number];
+
+    let mut counter = 0;
+
+    let mut underscore_word: Vec<String> = Vec::new();
+
+    loop {
+        if counter == word_to_be_guessed.len() {
+            break;
+        }
+
+        underscore_word.push("_".to_string());
+        counter += 1;
+    }
+
+    io::stdout().write(b"Start guessing:").expect("Failed");
+
+    while underscore_word.contains(&"_".to_string()) && maximum_player_lives > 0 {
+        println!("Remaining lifes: {:?} ", maximum_player_lives);
+
+        println!("{:?}", underscore_word);
+
+        let mut user_input_letter = String::new();
+
+        io::stdin()
+            .read_line(&mut user_input_letter)
+            .expect("Failed reading line");
+
+        let is_letter_in_word =
+            word_to_be_guessed.contains(*&user_input_letter.chars().next().unwrap());
+
+        if is_letter_in_word == false {
+            maximum_player_lives -= 1;
+            continue;
+        }
+
+        let index_of_letter_in_word =
+            word_to_be_guessed.match_indices(user_input_letter.chars().next().unwrap());
+
+        for val in index_of_letter_in_word {
+            let _value = std::mem::replace(
+                &mut underscore_word[val.0],
+                user_input_letter.chars().next().unwrap().to_string(),
+            );
+        }
+    }
+
+    if maximum_player_lives == 0 {
+        println!("YOU ARE HANGED!");
+        return;
+    }
+
+    println!("YOU WON!!!");
 }
